7ce34e80175ed3f7c2da874e136ac0a9f851227b
LIU CHIAWEI <doublecheap@gmail.com>
2025-09-22 12:04:32 +0900

    feat/ui: basic home page ui

---
+++ src/components/layout/canvas.tsx
@@ -0,0 +1,8 @@
+// 3d canvas
+export default function Canvas() {
+  return (
+    <div className="w-full h-full min-h-120 flex justify-center items-center bg-gray-300">
+      3D Canvas
+    </div>
+  );
+}
