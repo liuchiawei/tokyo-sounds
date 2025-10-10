46ee1efbbb4e220f31a1ce37a50e51e365e99cb0
LIU CHIAWEI <doublecheap@gmail.com>
2025-10-10 11:45:11 +0900

    fix: turn off eslint rule "@typescript-eslint/no-this-alias"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-this-alias": "off",
     },
   },
 ];
