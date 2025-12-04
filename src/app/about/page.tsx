ac6ebaf2bcf9ff4b45a970f4222aa6d2a1a0e173
LIU CHIAWEI <doublecheap@gmail.com>
2025-12-04 10:45:01 +0900

    feat: enhance About and City pages with new components and improved structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,14 @@
+import TiltShineCard from "@/components/common/TiltShineCard";
 import BackToHomeButton from "@/components/common/BackToHomeButton";
 
 export default function AboutPage() {
   return (
-    <>
+    <main>
       <h1>About Page</h1>
+      <div>
+        <TiltShineCard />
+      </div>
       <BackToHomeButton />
-    </>
+    </main>
   );
 }
